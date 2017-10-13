8773d601575f34571347f1f1267ab8d1a983cc72
Simon Rogers <s.rogers@waxdigital.com>
2017-10-13 09:03:33 +0100

    Initial commit of project

---
+++ books/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace books
+{
+    class Program
+    {
+        static string dest = @"C:\book_temp\";
+        static void Main(string[] args)
+        {
+            var source = @"D:\Other - Samsung Drive\kindle books";
+
+            DoDirectoryRecurse(source);
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+
+        private static void DoDirectoryRecurse(string sourcedir)
+        {
+            var files = Directory.GetFiles(sourcedir);
+            foreach (string fileposs in files)
+            {
+                var extension = Path.GetExtension(fileposs);
+                if (extension.Contains("mobi"))
+                {
+                    var filename = Path.GetFileName(fileposs);
+
+                    File.Copy(fileposs, dest + filename);
+                }
+            }
+
+            foreach (var direct in Directory.GetDirectories(sourcedir))
+            {
+                DoDirectoryRecurse(direct);
+            }
+        }
+    }
+}
